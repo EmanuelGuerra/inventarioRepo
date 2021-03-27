6c1cba2b61670d037a824b608523fa6d823f39fd
EmaunelGuerra <emanuelguerra9610@gmail.com>
2021-03-27 17:23:38 -0600

    Actualizacion de delete

--- crud2T/empleados/crud/eliminar-empleado.php
+++ crud2T/empleados/crud/eliminar-empleado.php
@@ -5,8 +5,10 @@
 	//$empleadoId = $_REQUEST["empleadoId"];
 	//$delete = mysqli_query($conexion,"DELETE FROM empleados WHERE empleadoId='$empleadoId'");
 	//header("Location: ../empleados.php");
-	<br>Aqui se cometo el Delete para poder crear softdelete<br>
-	<br>Aqui se cometo el Delete para poder crear softdelete<br>
-	<br>Aqui se cometo el Delete para poder crear softdelete<br>
-	<br>Aqui se cometo el Delete para poder crear softdelete<br>
+
+	// obtener inputs entre las etiquetas form
+	$empleadoId = $_REQUEST["empleadoId"];
+	$delete = mysqli_query($conexion,"UPDATE SET softdelete = 1 WHERE empleadoId='$empleadoId'");
+	header("Location: ../empleados.php");
+
 ?>
